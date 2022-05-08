de81b09d89d01d5f43269035c5fb2d74f8f174d2
nitinvdubey89 <nitinvdubey89@hotmail.com>
2022-05-08 21:49:20 +0400

    Changes to be committed: 	new file:   api and requests.py
     Changes to be committed:
    	new file:   api and requests.py
    
    Untracked files:
    	decoratorfile.py
    	decoratorspeedtest.py
    	project1/
    	pyats-coding-101/
            api and requests.py

---
+++ api and requests.py
@@ -0,0 +1,17 @@
+# requests is not a built in function
+import requests
+import json
+## used to make http request to web servers
+# this library does not come with python
+
+response  = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+todos = json.loads(response.text)
+
+print(todos)
+print(type(todos))
+
+for task in todos:
+    if task['completed'] == True:
+        # true should be True
+        print(task)
